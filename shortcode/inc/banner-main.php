9ee59023bbadcdd7375f7e8afafedcc1d6bcb29c
longpv <long05062000@gmail.com>
2025-06-28 15:57:49 +0700

    no message

--- shortcode/inc/banner-main.php
+++ shortcode/inc/banner-main.php
@@ -4,26 +4,37 @@
 // ===============================
 add_shortcode('tgb_banner_main', function () {
     ob_start();
+
+    $image_1 = get_field('banner_main_image_1', 'option') ?? '';
+    $image_2 = get_field('banner_main_image_2', 'option') ?? '';
+    $image_3 = get_field('banner_main_image_3', 'option') ?? '';
 ?>
     <div class="tgb_banner_main">
         <div class="grid_row">
-            <div class="grid_col-8">
-                <div class="img_wrap large">
-                    <img src="<?php echo TGB_IMG_URL . 'img6.png'; ?>" alt="">
-                </div>
+            <div class="grid_col-lg-8">
+                <?php if ($image_1) : ?>
+                    <div class="img_wrap large">
+                        <img src="<?php echo $image_1; ?>" alt="banner 1">
+                    </div>
+                <?php endif; ?>
             </div>
-            <div class="grid_col-4">
+            <div class="grid_col-lg-4">
                 <div class="grid_row">
-                    <div class="grid_col-12">
-                        <div class="img_wrap small">
-                            <img src="<?php echo TGB_IMG_URL . 'img7.png'; ?>" alt="">
+                    <?php if ($image_2) : ?>
+                        <div class="grid_col-12">
+                            <div class="img_wrap small">
+                                <img src="<?php echo $image_2; ?>" alt="banner 2">
+                            </div>
                         </div>
-                    </div>
-                    <div class="grid_col-12">
-                        <div class="img_wrap small">
-                            <img src="<?php echo TGB_IMG_URL . 'img8.png'; ?>" alt="">
+                    <?php endif; ?>
+
+                    <?php if ($image_3) : ?>
+                        <div class="grid_col-12">
+                            <div class="img_wrap small">
+                                <img src="<?php echo $image_3; ?>" alt="banner 3">
+                            </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
