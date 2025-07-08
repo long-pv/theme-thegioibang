28379dd54b77c44dfe3645811c6178e91b484220
longpv <long05062000@gmail.com>
2025-07-08 23:19:48 +0700

    no message

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -10,41 +11,38 @@ global $flatsome_opt;
 
 <div class="hotline-phone-ring-wrap">
     <div class="hotline-phone-ring">
-        <div class="hotline-phone-ring-circle"></div>
+        <!-- <div class="hotline-phone-ring-circle"></div>
         <div class="hotline-phone-ring-circle-fill"></div>
-        <div class="hotline-phone-ring-img-circle">
-        <a href="https://zalo.me/<?= the_field('cta_zalo_oa','option') ?>" target="_blank">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon_Zalo.png" alt="">
-        </a>
+        <div class="hotline-phone-ring-img-circle"> -->
+            <a href="https://zalo.me/<?= the_field('cta_zalo_oa', 'option') ?>" target="_blank">
+                <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon_Zalo.png" alt=""> -->
+                <img src="<?php echo TGB_IMG_URL . 'zalo.png' ?>" alt="">
+            </a>
         </div>
     </div>
-<!--     <div class="hotline-bar">
-        <a href="https://zalo.me/<?= the_field('cta_zalo_oa','option') ?>" target="_blank">
-            <span>Chat qua Zalo</span>
-        </a>
-    </div>
- --></div>
+</div>
 <div class="call">
-     <div class="hotline-phone-ring">
-        <div class="hotline-phone-ring-circle"></div>
+    <div class="hotline-phone-ring">
+        <!-- <div class="hotline-phone-ring-circle"></div>
         <div class="hotline-phone-ring-circle-fill"></div>
-        <div class="hotline-phone-ring-img-circle">
-        <div class="call_pop"><a id="callnowbutton" href="#popup_call"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/call.png" alt=""></a></div>
-        </div>
+        <div class="hotline-phone-ring-img-circle"> -->
+            <div class="call_pop">
+                <a id="callnowbutton" href="#popup_call">
+                    <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/call.png" alt=""> -->
+                    <img src="<?php echo TGB_IMG_URL . 'phone.png' ?>" alt="">
+                </a>
+            </div>
+        <!-- </div> -->
     </div>
-   
-  
-  
-  <!-- <div class="mess"><a id="message" href="https://m.me/<?= the_field('messenger','option') ?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/mess.png" alt=""></a></div> -->
 </div>
 <div id="popup_call" class="lightbox-by-id lightbox-content mfp-hide lightbox-white " style="max-width:500px ;padding:20px">
-    <?= the_field('list_phone','option') ?>
+    <?= the_field('list_phone', 'option') ?>
 </div>
 </main><!-- #main -->
 
 <footer id="footer" class="footer-wrapper">
 
-	<?php do_action('flatsome_footer'); ?>
+    <?php do_action('flatsome_footer'); ?>
 
 </footer><!-- .footer-wrapper -->
 <!-- <script type="text/javascript">
@@ -59,4 +57,5 @@ global $flatsome_opt;
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
